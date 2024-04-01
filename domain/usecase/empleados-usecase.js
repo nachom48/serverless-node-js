1bf99f1cd39ae398445d2460d1f93babe2141df4
nachom48 <nachom48@gmail.com>
2024-03-31 23:20:19 -0300

    adding connection

--- domain/usecase/empleados-usecase.js
+++ domain/usecase/empleados-usecase.js
@@ -2,6 +2,10 @@ const { HttpError } = require('../../application/exceptions/http-error');
 const empleadoAdapter = require('../../application/helpers/model_adapters/empleado-adapter')
 const {StatusCodes} = require('http-status-codes')
 
+//rest Client
+
+const axios = require('axios');
+
 const getEmpleados = async () => {
     return empleadoAdapter.findEmpleados();
 }
@@ -24,6 +28,14 @@ const deleteEmpleado = async (id) => {
     await empleadoAdapter.deleteEmpleado(id);
 }
 const getDetailEmpleado = async (id) => {
+
+    const empleado = await empleadoAdapter.findOneEmpleado(id);
+    if (empleado === null){
+        throw new HttpError("Empleado no existe",StatusCodes.NOT_FOUND)
+    }
+
+
+    const historicos = await getHistoricoEmpleados(id);
     await validateExisteEmpleado(id);
         return empleado;
 
@@ -43,6 +55,23 @@ const validateExisteEmpleado = async (id) =>{
     if (empleado === null){
         throw new HttpError('Empleado no existe',StatusCodes.NOT_FOUND)}
 }
+
+//Migrar
+
+const getHistoricoEmpleados = async(id) =>{
+
+    try {
+        const result = await axios(process.env.SERVICE_HISTORICO+'/historicos/'+id,{
+            headers: {
+                'x-api-key':process.env.SERVICE_HISTORICO_KEY
+            }
+        });
+    } catch (error) {
+        console.log("getHistoricoEmpleado",error.response.status);
+        console.log('getHistoricoEmpleado',error.response.data)
+    }
+}
+
 
 module.exports = {
     getEmpleados,
